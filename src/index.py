9e85487267d56ce3231409948a57f4e3e8394057
marcom1994 <marco.minervino94@gmail.com>
2022-06-11 12:32:12 +0200

    test for vercel

---
+++ src/index.py
@@ -0,0 +1,11 @@
+from flask import Flask
+from projects.cryptoTracking.src.launcher.CryptoTrackingMain import CryptoTrackingMain
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    cryptoTrackingLauncher = CryptoTrackingMain()
+    return cryptoTrackingLauncher.main()
+
